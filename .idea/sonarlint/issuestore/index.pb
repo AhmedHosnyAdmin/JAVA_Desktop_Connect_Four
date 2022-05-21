c409f86b60ccfc5bb4325bcf5b0ee82c8672c0e4
ahmedhosnypro <ahmedhosnypro@gmail.com>
2022-05-22 00:05:09 +0200

    create diagonals

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+h
+8Desktop Connect Four/Setting up the board/task-info.yaml,7\e\7e302dfdaf69d10c8326042b70f811536a86c526
+f
+6Desktop Connect Four/Placing the pieces/task-info.yaml,8\6\86a4576b258d58d108f7af6a42c9e6f81ef350cc
--- Desktop Connect Four/task/src/four/ConnectFour.java
+++ Desktop Connect Four/task/src/four/ConnectFour.java
@@ -9,8 +9,13 @@ import java.util.Comparator;
 public class ConnectFour extends JFrame {
     JPanel board = new JPanel(new GridLayout(6, 7, 0, 0));
     char currentTurn = 'X';
-    ArrayList<ArrayList<JButton>> jButtonColumns = new ArrayList<>();
+    ArrayList<ArrayList<JButton>> columns = new ArrayList<>();
+    ArrayList<ArrayList<JButton>> rows = new ArrayList<>();
+    ArrayList<ArrayList<JButton>> mainDiagonals = new ArrayList<>();
+    ArrayList<ArrayList<JButton>> sideDiagonals = new ArrayList<>();
+    ArrayList<JButton> buttons = new ArrayList<>();
     private final Dimension buttonDimensions = new Dimension(98, 98);
+    private final String BUTTONS = "Button";
 
 
     public ConnectFour() {
@@ -27,20 +32,26 @@ public class ConnectFour extends JFrame {
 
     private void addBoard() {
         createButtons();
+        createMainDiagonals();
+        createSideDiagonals();
         add(board);
         addAction();
     }
 
     private void createButtons() {
         for (int i = 0; i < 7; i++) {
-            jButtonColumns.add(new ArrayList<>());
+            columns.add(new ArrayList<>());
         }
 
-        for (int row = 6; row >= 1; row--) {
+        for (int i = 0; i < 6; i++) {
+            rows.add(new ArrayList<>());
+        }
+        int row = 0;
+        for (int rowReversed = 6; rowReversed >= 1; rowReversed--) {
             for (char col = 'A'; col <= 'G'; col++) {
-                JButton button = new JButton(" ");
+                JButton button = new JButton(col + "" + rowReversed);
                 button.setFocusPainted(false);
-                button.setName("Button" + col + "" + row);
+                button.setName(BUTTONS + col + "" + rowReversed);
                 button.setBackground(Color.BLACK);
                 button.setForeground(Color.CYAN);
                 button.setBorder(new LineBorder(Color.DARK_GRAY, 1, true));
@@ -48,22 +59,25 @@ public class ConnectFour extends JFrame {
                 button.setPreferredSize(buttonDimensions);
                 button.setMaximumSize(buttonDimensions);
                 button.setMinimumSize(buttonDimensions);
-                jButtonColumns.get(col - 65).add(button);
+                buttons.add(button);
+                rows.get(row).add(button);
+                columns.get(col - 65).add(button);
                 board.add(button);
             }
+            row++;
         }
 
-        for (var col : jButtonColumns) {
+        for (var col : columns) {
             col.sort(Comparator.comparing(Component::getName));
         }
     }
 
     private void addAction() {
-        for (var col : jButtonColumns) {
+        for (var col : columns) {
             for (var button : col) {
                 button.addActionListener(e -> {
                     System.out.println(button.getName());
-                    var currentColumn = jButtonColumns.get(button.getName().charAt(6) - 65);
+                    var currentColumn = columns.get(button.getName().charAt(6) - 65);
                     addTextToButtons(currentColumn);
                 });
             }
@@ -81,6 +95,52 @@ public class ConnectFour extends JFrame {
                     currentTurn = 'X';
                 }
                 break;
+            }
+        }
+    }
+
+    private void createMainDiagonals() {
+        for (var row : rows) {
+            ArrayList<JButton> diagonal = new ArrayList<>();
+            int rowNumReversed = row.get(0).getName().charAt(7) - 48;
+            char col = 'A';
+            if (rowNumReversed > 3) {
+                int rowNum = rowNumReversed + (6 - 2 * rowNumReversed);
+                while (rowNumReversed >= 1) {
+                    int finalRowNumReversed = rowNumReversed;
+                    char finalCol = col;
+                    var optionalButton = rows.get(rowNum).stream().filter(b -> b.getName().equals(BUTTONS + finalCol + "" + finalRowNumReversed)).findFirst();
+                    optionalButton.ifPresent(diagonal::add);
+                    rowNumReversed--;
+                    rowNum++;
+                    col++;
+                }
+            }
+            if (diagonal.size() >= 4) {
+                mainDiagonals.add(diagonal);
+            }
+        }
+    }
+
+    private void createSideDiagonals() {
+        for (var row : rows) {
+            ArrayList<JButton> diagonal = new ArrayList<>();
+            int rowNumReversed = row.get(row.size() - 1).getName().charAt(7) - 48;
+            char col = 'G';
+            if (rowNumReversed < 4) {
+                int rowNum = rowNumReversed + (6 - 2 * rowNumReversed);
+                while (rowNumReversed <= 6) {
+                    int finalRowNumReversed = rowNumReversed;
+                    char finalCol = col;
+                    var optionalButton = rows.get(rowNum).stream().filter(b -> b.getName().equals(BUTTONS + finalCol + "" + finalRowNumReversed)).findFirst();
+                    optionalButton.ifPresent(diagonal::add);
+                    rowNumReversed++;
+                    rowNum--;
+                    col--;
+                }
+            }
+            if (diagonal.size() >= 4) {
+                sideDiagonals.add(diagonal);
             }
         }
     }
